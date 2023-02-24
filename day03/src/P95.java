283ae0978bc794ba3790fc6dc1823829d1ced047
David Byun <byundavid@naver.com>
2023-02-24 17:16:59 +0900

    0224 java 제어문 조건문 마무리

--- day03/src/P95.java
+++ day03/src/P95.java
@@ -1,19 +1,23 @@
-public class P94 {
+public class P95 {
     public static void main(String[] args) {
         //짝수만 구하시오
         //합과 평균을 구하시오
         int sum = 0;
-        double avg = 0.0;
+        double avg = 0;
         int cnt = 0;
-        for (int i = 1; i <= 100; i++) {
-            if (i % 2 != 0) {
-                continue;
+        int i = 1;
+        while (i <= 10) {
+            if (i % 2 == 0){
+                sum += i;
+                cnt++;
             }
-            sum += i;
-            cnt++;
+            if (i == 8) {
+                break;
+            }
+            i++;
+
         }
-        avg = sum / (cnt * 1.0);
-        System.out.println("cnt = " + cnt);
+        avg = (sum*1.0)/ cnt;
         System.out.println("avg = " + avg);
         System.out.println("sum = " + sum);
 
