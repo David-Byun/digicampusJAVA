5db803ab57610e2514a7e83694b5c78015aa065e
David Byun <byundavid@naver.com>
2023-02-27 15:11:53 +0900

    branch 교체 위한 셋팅

--- day03/src/ifcontrole/ws.java
+++ day03/src/ifcontrole/ws.java
@@ -7,50 +7,60 @@ public class ws {
         // 2. 4의 배수의 합과 평균을 구하시오
         // 3. 7의 배수의 합과 평균을 구하시오
 
-        /*
-        double avg = 0;
-        int sum = 0;
-        int cnt = 0;
+
+        double avg1 = 0;
+        int sum1 = 0;
+        int cnt1 = 0;
         for (int i = 1; i <= 100; i++) {
             if (i % 3 == 0) {
-                sum += i;
-                cnt++;
+                sum1 += i;
+                cnt1++;
             }
         }
-        avg = sum / (cnt * 1.0);
-        System.out.println("3의 배수 합 = " + sum);
-        System.out.println("3의 배수 평균 = " + avg); */
+        avg1 = sum1 / (cnt1 * 1.0);
+        System.out.println("3의 배수 합 = " + sum1);
+        System.out.println("3의 배수 평균 = " + avg1);
+        System.out.println("3의 배수 개수 = " + cnt1);
 
-        /*
-        double avg = 0;
-        int sum = 0;
-        int cnt = 0;
-        int i = 1;
-        while (i <= 100) {
-            if (i % 4 == 0) {
-                sum += i;
-                cnt++;
-            }
-            i++;
-        }
-        avg = (sum * 1.0) / cnt;
-        System.out.println("4의 배수 합 = " + sum);
-        System.out.println("4의 배수 평균 = " + avg); */
 
-        double avg = 0;
-        int sum = 0;
-        int cnt = 0;
-        int i = 1;
-        while (i <= 100) {
-            if (i % 7 == 0) {
-                sum += i;
-                cnt++;
+        double avg2 = 0;
+        int sum2 = 0;
+        int cnt2 = 0;
+        int i2 = 1;
+        while (i2 <= 100) {
+            if (i2 % 4 == 0) {
+                sum2 += i2;
+                cnt2++;
             }
-            i++;
+            i2++;
         }
-        avg = (sum * 1.0) / cnt;
-        System.out.println("7의 배수 합 = " + sum);
-        System.out.println("7의 배수 평균 = " + avg);
-        System.out.println("7의 배수 개수 = " + cnt);
+        avg2 = (sum2 * 1.0) / cnt2;
+        System.out.println("4의 배수 합 = " + sum2);
+        System.out.println("4의 배수 평균 = " + avg2);
+        System.out.println("4의 배수 개수 = " + cnt2);
+
+        double avg3 = 0;
+        int sum3 = 0;
+        int cnt3 = 0;
+        int i3 = 1;
+        while (i3 <= 100) {
+            if (i3 % 7 == 0) {
+                sum3 += i3;
+                cnt3++;
+            }
+            i3++;
+        }
+        avg3 = (sum3 * 1.0) / cnt3;
+        System.out.println("7의 배수 합 = " + sum3);
+        System.out.println("7의 배수 평균 = " + avg3);
+        System.out.println("7의 배수 개수 = " + cnt3);
+
+
+        for(int i=2;i<10;i++) {
+            System.out.printf("%d 단 \n",i);
+            for(int j=1;j<10;j++) {
+                System.out.printf("%d * %d = %d \n",i,j,(i*j));
+            }
+        }
     }
 }
