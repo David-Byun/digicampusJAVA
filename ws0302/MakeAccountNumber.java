373f17aa0cdd478cf4e7b46b81212d9c3e6c1b51
David Byun <byundavid@naver.com>
2023-03-02 17:10:13 +0900

    0302 자바 과제 최종 제출

--- ws0302/MakeAccountNumber.java
+++ ws0302/MakeAccountNumber.java
@@ -3,7 +3,6 @@ import java.util.Calendar;
 public class MakeAccountNumber {
     public static String makeAccNum() {
         String accNum = "";
-        double value = Math.random();
         Calendar cal = Calendar.getInstance();
         accNum = "" + cal.get(Calendar.YEAR) + (cal.get(Calendar.MONTH) + 1) + cal.get(Calendar.DAY_OF_YEAR) + cal.get(Calendar.MILLISECOND) +  (int)(Math.random()*1000 + 1);
         return accNum;
